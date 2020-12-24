6b5283ed3411d28b29b70e41b38253becbcfacc1
copiner <wdaonngg@gmail.com>
2020-12-24 10:30:35 +0800

    brower

--- src/hook/effect/sample.js
+++ src/hook/effect/sample.js
@@ -1,3 +1,15 @@
+/*
+在浏览器完成布局与绘制之后，传给 useEffect 的函数会延迟调用
+
+然而，并非所有 effect 都可以被延迟执行。
+例如，在浏览器执行下一次绘制前，用户可见的 DOM 变更就必须同步执行，这样用户才不会感觉到视觉上的不一致
+React为此提供了useLayoutEffect Hook来处理这类 effect。
+它和 useEffect 的结构相同，区别只是调用时机不同
+
+
+虽然 useEffect 会在浏览器绘制后延迟执行，但会保证在任何新的渲染前执行。
+React 将在组件更新前刷新上一轮渲染的 effect
+*/
 import React from 'react';
 
 import { useState, useEffect } from 'react';
